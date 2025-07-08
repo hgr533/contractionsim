5b6870051b96306dc0b94d12bf9faeabc55510e9
hagr533 <30835065+hgr533@users.noreply.github.com>
2025-07-08 20:19:02 +0300

    Add files via upload

---
+++ ContractionSim4/TPair.cs
@@ -0,0 +1,12 @@
+﻿
+public class TPair<T1, T2>
+{
+    private List<TzimtzumSimulation.Vector2> path;
+    private object value;
+
+    public TPair(List<TzimtzumSimulation.Vector2> path, object value)
+    {
+        this.path = path;
+        this.value = value;
+    }
+}
